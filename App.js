f80be3b6f7926906079ad540c520ce1aaf4a0cf0
SriwanKm <kcqkoo@gmail.com>
2021-04-13 10:11:01 -0700

    add border, sections, image

--- App.js
+++ App.js
@@ -1,48 +1,259 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  TouchableHighlight,
-  TouchableOpacity,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
+    TouchableHighlight,
+    TouchableOpacity,
+    ImageBackground,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
 } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+
+const image = {uri: "https://www.html.am/templates/downloads/bryantsmith/barrensavannah/mainImage.jpg"};
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LinearGradient colors={['#dff8fd', '#94c2cc']} style={{flex: 1}}>
-        <FlatList
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            style={{flex: 1}}
-            data={[
-              {key: 'Home'},
-              {key: 'About'},
-              {key: 'Portfolio'},
-              {key: 'Services'},
-              {key: 'Contact'},
-            ]}
-            renderItem={({item}) => (
-                <View style={styles.navItem}>
-                  <TouchableHighlight underlayColor="#94c2cc" onPress={pressed}>
-                    <Text style={styles.navText}>{item.key}</Text>
-                  </TouchableHighlight>
-                </View>
-            )}
-        />
-      </LinearGradient>
-    </View>
-  );
+    const [navMenu, setNav] = useState([
+        {key: 'Home', id: 1, isPressed: false},
+        {key: 'About', id: 2, isPressed: false},
+        {key: 'Portfolio', id: 3, isPressed: false},
+        {key: 'Services', id: 4, isPressed: false},
+        {key: 'Contact', id: 5, isPressed: false},
+    ])
+
+    const pressIn = (id) => {
+        setNav({
+            navMenu: navMenu.map(el => {
+                if (el.id === id) {
+                    navMenu.isPressed = true;
+                }
+                console.log(navMenu)
+                return navMenu;
+            })
+        })
+    };
+
+    const pressOut = (id) => {
+        setNav({
+            navMenu: navMenu.map(el => {
+                if (el.id === id) {
+                    navMenu.isPressed = false;
+                }
+                return navMenu;
+            })
+        })
+    };
+
+    return (
+        <ScrollView>
+            <LinearGradient colors={['#a63702', '#eb8600']} style={styles.navContainer}>
+                <FlatList
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={navMenu.id}
+                    style={{flex: 1}}
+                    data={navMenu}
+                    renderItem={({item}) => (
+                        <View style={styles.navItem}>
+                            <TouchableHighlight underlayColor="#000000" onPressIn={() => pressIn(item.id)}
+                                                onPressOut={() => pressOut(item.id)}>
+                                <Text
+                                    style={item.isPressed ? styles.navTextDark : styles.navTextLight}>{item.key}</Text>
+                            </TouchableHighlight>
+                        </View>
+                    )}
+                />
+            </LinearGradient>
+            <View style={styles.line}></View>
+            <LinearGradient colors={['#eb8600', '#4f2108', '#26070a']} style={styles.sectionCon}>
+
+                <ImageBackground source={image} style={styles.img}>
+                    <Text style={styles.header}>Barren Savannah</Text>
+                    <Text style={styles.subHeader}>An XHTML 1.0 Strict Template by Bryant Smith</Text>
+                </ImageBackground>
+
+                <LinearGradient colors={['#fed5ac', '#ffffff', '#ffffff', '#ffffff']} style={styles.section}>
+                    <View style={styles.subSection}>
+                        <Text style={styles.sectionHeader}>The Title of an Article</Text>
+                        <Text style={styles.p}>
+                            You may use this template on any site, anywhere, for free just please leave the link back to
+                            me
+                            in the footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :)
+                            This is
+                            what a link looks like.
+                        </Text>
+
+                        <Text style={styles.p}>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque
+                            vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor
+                            sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet
+                            imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis
+                            risus
+                            ut felis. Sed vehicula pellentesque quam.
+                        </Text>
+                        <View style={styles.dottedBox}>
+                            <Text style={styles.p}>
+                                This is a block quote, use it to include quotes from yourself or others. All you have to
+                                do to include this element is wrap some text around blockquote tags
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.subSection}>
+                        <Text style={styles.sectionHeader}>Titles are H1 Tags</Text>
+                        <Text style={styles.p}>
+                            Since the titles are H1 tags, try to include your keywords in them as search engines will
+                            look
+                            at them as being important content.
+                        </Text>
+
+                        <Text style={styles.p}>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque
+                            vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor
+                            sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet
+                            imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis
+                            risus
+                            ut felis. Sed vehicula pellentesque quam.
+                        </Text>
+
+                        <Text style={styles.p}>
+                            Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue
+                            tortor,
+                            tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus
+                            quis,
+                            laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna.
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad
+                            litora
+                            torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis,
+                            pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.
+                        </Text>
+                    </View>
+
+                    <View style={styles.subSection}>
+                        <Text style={styles.sectionHeader}>Yet Another One!</Text>
+                        <Text style={styles.p}>
+                            You may use this template on any site, anywhere, for free just please leave the link back to
+                            me
+                            in the footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :)
+                        </Text>
+
+                        <Text style={styles.p}>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque
+                            vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor
+                            sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet
+                            imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis
+                            risus
+                            ut felis. Sed vehicula pellentesque quam.
+                        </Text>
+
+                        <Text style={styles.p}>
+                            Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue
+                            tortor,
+                            tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus
+                            quis,
+                            laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna.
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad
+                            litora
+                            torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis,
+                            pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.
+                        </Text>
+
+                    </View>
+                </LinearGradient>
+            </LinearGradient>
+
+        </ScrollView>
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    navContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 40,
+        paddingBottom: 25,
+
+    },
+    navTextLight: {
+        flex: 1,
+        paddingHorizontal: 35,
+        color: '#000000',
+        paddingVertical: 18,
+        fontSize: 16,
+        fontWeight: 'bold',
+
+    },
+    navTextDark: {
+        flex: 1,
+        paddingHorizontal: 35,
+        color: '#ffffff',
+        paddingVertical: 18,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    img: {
+        borderColor: '#000000',
+        borderWidth: 9,
+        borderTopWidth: 0,
+        marginHorizontal: 20,
+        marginBottom: 20,
+        paddingBottom: 360,
+    },
+    header: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+        fontSize: 26,
+        marginHorizontal: 20,
+        paddingTop: 12,
+    },
+    subHeader: {
+        color: '#000000',
+        fontSize: 18,
+        marginHorizontal: 20,
+        paddingBottom: 12,
+    },
+    line: {
+        borderTopColor: '#000000',
+        borderTopWidth: 12,
+    },
+    sectionCon: {},
+    dottedBox: {
+        borderColor: '#c97003',
+        borderWidth: 1,
+        margin: 20,
+        padding: 15,
+        borderStyle: 'dashed',
+        width: '70%',
+    },
+    section: {
+        borderColor: '#000000',
+        borderWidth: 9,
+        margin: 20,
+        paddingHorizontal: 10,
+    },
+    subSection: {
+        padding: 12,
+    },
+    sectionHeader: {
+        fontWeight: 'bold',
+        fontSize: 26,
+        marginBottom: 8,
+    },
+    p: {
+        fontSize: 16,
+        marginVertical: 10,
+    },
 });
